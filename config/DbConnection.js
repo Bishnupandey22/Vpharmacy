2d333aa3dfaf267f7c8556b94f59be214aabe68d
Bishnu <bishnu@spktechnosoft.in>
2024-07-15 11:05:33 +0530

    changes in database url

--- config/DbConnection.js
+++ config/DbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const connectDb = async (DATABASE_URL) => {
     try {
         const DB_OPTION = {
-            dbName: 'vPharmacy'
+            dbName: 'vPharmacy',
         }
         await mongoose.connect(DATABASE_URL, DB_OPTION);
         console.log("mongodb connected Sucessfully")
